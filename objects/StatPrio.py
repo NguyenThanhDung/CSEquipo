2d44a2f365b54c4196341840a4d03799b0fa0022
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-07-29 22:46:29 +0700

    Convert string to StatisticType

--- objects/StatPrio.py
+++ objects/StatPrio.py
@@ -1,9 +1,28 @@
+from Defines import StatisticType
 
 class StatPrio:
 
     def __init__(self, guardian, prioStat):
         self.guardian = guardian
-        self.prioStat = prioStat
+        self.prioStat = self.StatNameToStatType(prioStat)
+
+    def StatNameToStatType(self, statName):
+        if statName == "ATK":
+            return StatisticType.Attack
+        elif statName == "DEF":
+            return StatisticType.Defend
+        elif statName == "PincerATK":
+            return StatisticType.PincerAttack
+        elif statName == "HP":
+            return StatisticType.HP
+        elif statName == "CRT Rate":
+            return StatisticType.CrtRate
+        elif statName == "CRT Dmg":
+            return StatisticType.CrtDmg
+        elif statName == "ACC":
+            return StatisticType.Accuracy
+        elif statName == "RES":
+            return StatisticType.Resistance
 
     def ToString(self):
         return self.guardian.ljust(15) + ": " + str(self.prioStat)
