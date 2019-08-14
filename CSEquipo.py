e1ae21d61dd98020e5742534d4c3921cf4f8255c
Nguyen Thanh Dung (Mac) <dung.nguyenthanh@gameloft.com>
2019-08-14 19:33:21 +0700

    + Add placeholder for new optimizing function

--- CSEquipo.py
+++ CSEquipo.py
@@ -7,57 +7,60 @@ from objects.EquipmentSet import EquipmentSet
 from objects.Guardian import Guardian
 
 
+def Optimize1(guardianList, equipmentList):
+    for guardian in guardianList:
+        print("Find best equipment for " + guardian.name + ":")
+        print("Generate equipment sets")
+        equipmentSets = equipmentList.GenerateEquipmentSet()
+        maxStatValue = 0
+        maxEquipmentSet = None
+        for equipmentSet in equipmentSets:
+            print("Try set " + equipmentSet.ToString())
+            guardian.UnequipAll()
+            guardian.Equip(equipmentSet.weapon)
+            guardian.Equip(equipmentSet.armor)
+            guardian.Equip(equipmentSet.shield)
+            guardian.Equip(equipmentSet.gloves)
+            guardian.Equip(equipmentSet.necklace)
+            guardian.Equip(equipmentSet.ring)
+            finalStats = guardian.CalculateFinalStats()
+            desireStat = statPrioMap.GetDesireStat(guardian)
+            if desireStat == StatisticType.Attack:
+                averageAttack = Guardian.GetAverageAttack(finalStats)
+                print("Desire stat value: " + str(averageAttack))
+                if averageAttack > maxStatValue:
+                    maxStatValue = averageAttack
+                    maxEquipmentSet = equipmentSet
+            else:
+                print("Desire stat value: " + str(finalStats[desireStat]))
+                if finalStats[desireStat] > maxStatValue:
+                    maxStatValue = finalStats[desireStat]
+                    maxEquipmentSet = equipmentSet
+            print("Current best stat: " + str(maxStatValue))
+        guardian.UnequipAll()
+        guardian.Equip(maxEquipmentSet.weapon)
+        guardian.Equip(maxEquipmentSet.armor)
+        guardian.Equip(maxEquipmentSet.shield)
+        guardian.Equip(maxEquipmentSet.gloves)
+        guardian.Equip(maxEquipmentSet.necklace)
+        guardian.Equip(maxEquipmentSet.ring)
+        equipmentList.Remove(maxEquipmentSet.weapon)
+        equipmentList.Remove(maxEquipmentSet.armor)
+        equipmentList.Remove(maxEquipmentSet.shield)
+        equipmentList.Remove(maxEquipmentSet.gloves)
+        equipmentList.Remove(maxEquipmentSet.necklace)
+        equipmentList.Remove(maxEquipmentSet.ring)
+    return None
+
+def Optimize2(guardianList, equipmentList):
+    return None
+
+
 def main():
     equipmentList = EquipmentList("data/equipments.json")
     guardianList = GuardianList("data/simplifiedGuardians.json", True)
-
-    # for guardian in guardianList:
-    #     print("Find best equipment for " + guardian.name + ":")
-    #     print("Generate equipment sets")
-    #     equipmentSets = equipmentList.GenerateEquipmentSet()
-    #     maxStatValue = 0
-    #     maxEquipmentSet = None
-    #     for equipmentSet in equipmentSets:
-    #         print("Try set " + equipmentSet.ToString())
-    #         guardian.UnequipAll()
-    #         guardian.Equip(equipmentSet.weapon)
-    #         guardian.Equip(equipmentSet.armor)
-    #         guardian.Equip(equipmentSet.shield)
-    #         guardian.Equip(equipmentSet.gloves)
-    #         guardian.Equip(equipmentSet.necklace)
-    #         guardian.Equip(equipmentSet.ring)
-    #         finalStats = guardian.CalculateFinalStats()
-    #         desireStat = statPrioMap.GetDesireStat(guardian)
-    #         if desireStat == StatisticType.Attack:
-    #             averageAttack = Guardian.GetAverageAttack(finalStats)
-    #             print("Desire stat value: " + str(averageAttack))
-    #             if averageAttack > maxStatValue:
-    #                 maxStatValue = averageAttack
-    #                 maxEquipmentSet = equipmentSet
-    #         else:
-    #             print("Desire stat value: " + str(finalStats[desireStat]))
-    #             if finalStats[desireStat] > maxStatValue:
-    #                 maxStatValue = finalStats[desireStat]
-    #                 maxEquipmentSet = equipmentSet
-    #         print("Current best stat: " + str(maxStatValue))
-    #     guardian.UnequipAll()
-    #     guardian.Equip(maxEquipmentSet.weapon)
-    #     guardian.Equip(maxEquipmentSet.armor)
-    #     guardian.Equip(maxEquipmentSet.shield)
-    #     guardian.Equip(maxEquipmentSet.gloves)
-    #     guardian.Equip(maxEquipmentSet.necklace)
-    #     guardian.Equip(maxEquipmentSet.ring)
-    #     equipmentList.Remove(maxEquipmentSet.weapon)
-    #     equipmentList.Remove(maxEquipmentSet.armor)
-    #     equipmentList.Remove(maxEquipmentSet.shield)
-    #     equipmentList.Remove(maxEquipmentSet.gloves)
-    #     equipmentList.Remove(maxEquipmentSet.necklace)
-    #     equipmentList.Remove(maxEquipmentSet.ring)
-    # print("Final result:")
-
     for guardian in guardianList.guardians:
         print(guardian.ToString())
-
 
 if __name__ == "__main__":
     main()
--- objects/SimplifiedGuardian.py
+++ objects/SimplifiedGuardian.py
@@ -1,8 +1,10 @@
+from objects.Defines import EquipmentType
 from objects.Defines import GuardianDirection
 
-class SimplifiedGuardian:
 
-    def __init__(self, id, name, direction, baseStars, currentStars, level, status, priorityStatistic, note):
+class SimplifiedGuardian:
+    def __init__(self, id, name, direction, baseStars, currentStars, level,
+                 status, priorityStatistic, note):
         self.id = int(id)
         self.name = name
         self.direction = direction
@@ -12,6 +14,18 @@ class SimplifiedGuardian:
         self.status = status
         self.priorityStatistic = priorityStatistic
         self.note = note
+        self.equipments = {
+            EquipmentType.Weapon: 0,
+            EquipmentType.Armor: 0,
+            EquipmentType.Shield: 0,
+            EquipmentType.Gloves: 0,
+            EquipmentType.Necklace: 0,
+            EquipmentType.Ring: 0
+        }
 
     def ToString(self):
-        return "Guardian #" + str(self.id) + ": " + str(self.name) + "\n"
+        text = "Guardian #" + str(self.id) + ": " + str(self.name).ljust(10) + "| "
+        for equipmentID in self.equipments.values():
+            text += str(equipmentID).rjust(3)
+        text += "\n"
+        return text
